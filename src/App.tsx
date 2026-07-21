eb27e544613aed30964ba0cc4e6ea33e03c24fe3
kusaytgn-ux <kusay.tgn@gmail.com>
2026-07-21 18:27:10 +0300

    Описание внесенных изменений

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,52 @@
 import AppRouter from "./router/AppRouter";
 
+// В самом верху app.tsx
+declare global {
+  interface Window {
+    Telegram: {
+      WebApp: {
+        ready: () => void;
+        disableVerticalSwipes: () => void;
+        expand: () => void;
+        close: () => void;
+      };
+    };
+  }
+}
+import React, { useEffect } from 'react';
+
 function App() {
+  // ===== ДОБАВЬ ЭТОТ БЛОК =====
+  useEffect(() => {
+    const tg = window.Telegram.WebApp;
+    tg.ready();
+    tg.disableVerticalSwipes();
+    tg.expand();
+  }, []);
+  // ===== КОНЕЦ БЛОКА =====
+
+    const handleClose = () => {
+    window.Telegram.WebApp.close();
+  };
+  return (
+    <div>
+      {/* ... твой существующий код ... */}
+      
+      {/* Кнопку закрытия добавь в любое место */}
+      <button onClick={handleClose}>Закрыть</button>
+    </div>
+  );
   return <AppRouter />;
+  
 }
 
+
+
 export default App;
+
+
+
+
+
+
+
