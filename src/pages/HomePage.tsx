f33d91cd484c1a9e889573d89521aabd313392be
Ярослав <yaroslav@MacBook-Air-Aroslav.local>
2026-09-03 18:03:31 +0300

    first commit

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,35 +1,26 @@
 import Header from "../components/layout/Header";
-
 import OfferCard from "../components/OfferCard";
 import WeeklyProducts from "../components/WeeklyProducts";
 import BottomNavigation from "../components/navigation/BottomNavigation";
 import UserCard from "../components/sections/UserCard";
-
-
-
 import NewsSection from "../components/NewsSection";
 
 function HomePage() {
-  
   return (
-    <div className="min-h-screen bg-black pb-28">
-
+    <div className="min-h-screen bg-black pb-28 text-white">
       <Header />
 
       <main className="mx-auto max-w-md space-y-6 px-5 py-5">
-
         <UserCard />
-      
 
         <OfferCard />
 
         <WeeklyProducts />
-        <NewsSection />
 
+        <NewsSection />
       </main>
 
       <BottomNavigation />
-
     </div>
   );
 }
